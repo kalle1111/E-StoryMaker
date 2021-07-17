da1855647067e0136f3dd16cb901195486ef1889
Simon Liehr <simon.liehr@stud.hs-flensburg.de>
2021-07-17 10:44:26 +0200

    View: LoginFragment layout stuff

--- app/src/main/java/hsfl/project/e_storymaker/views/activities/AuthActivity.kt
+++ app/src/main/java/hsfl/project/e_storymaker/views/activities/AuthActivity.kt
@@ -2,13 +2,18 @@ package hsfl.project.e_storymaker.views.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import hsfl.project.e_storymaker.R
 import hsfl.project.e_storymaker.databinding.AuthActivityBinding
 import hsfl.project.e_storymaker.views.fragments.LoginFragment
 
 class AuthActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: AuthActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,11 +22,26 @@ class AuthActivity : AppCompatActivity() {
         binding = AuthActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbar)
+
+        //TextView textView = (TextView)binding.toolbar.findViewById(R.id.too)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_auth)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
         /*
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, LoginFragment.newInstance())
                 .commitNow()
         }*/
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_auth)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/hsfl/project/e_storymaker/views/fragments/RegisterFragment.kt
+++ app/src/main/java/hsfl/project/e_storymaker/views/fragments/RegisterFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import hsfl.project.e_storymaker.viewModels.AuthVM
 import hsfl.project.e_storymaker.R
@@ -36,9 +37,6 @@ class RegisterFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.toLoginButton.setOnClickListener{
-            findNavController().navigate(R.id.action_RegisterFragment_to_LoginFragment)
-        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {

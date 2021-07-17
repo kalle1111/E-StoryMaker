96705541649e7a09089e8dd58dffffa7274a23cf
Simon Liehr <simon.liehr@stud.hs-flensburg.de>
2021-07-17 10:55:45 +0200

    View: Hide nav/actionbar for loginFragment

--- app/src/main/java/hsfl/project/e_storymaker/views/activities/AuthActivity.kt
+++ app/src/main/java/hsfl/project/e_storymaker/views/activities/AuthActivity.kt
@@ -29,6 +29,7 @@ class AuthActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_auth)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
+        supportActionBar?.hide()
 
         /*
         if (savedInstanceState == null) {
@@ -41,6 +42,7 @@ class AuthActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_auth)
+        supportActionBar?.hide()
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
     }

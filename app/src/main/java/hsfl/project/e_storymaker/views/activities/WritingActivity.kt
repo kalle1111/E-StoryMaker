93f56d156de948394262cce1e73dde74bbb99ca5
Simon Liehr <simon.liehr@stud.hs-flensburg.de>
2021-07-30 12:08:46 +0200

    View: WriteActivity(WriteOverview, rough)

--- app/src/main/java/hsfl/project/e_storymaker/views/activities/WritingActivity.kt
+++ app/src/main/java/hsfl/project/e_storymaker/views/activities/WritingActivity.kt
@@ -2,18 +2,33 @@ package hsfl.project.e_storymaker.views.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import hsfl.project.e_storymaker.R
+import hsfl.project.e_storymaker.databinding.WritingActivityBinding
 import hsfl.project.e_storymaker.views.fragments.WriteDetailsFragment
 
 class WritingActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: WritingActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.writing_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, WriteDetailsFragment.newInstance())
-                .commitNow()
-        }
+
+        binding = WritingActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_write)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_write)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }

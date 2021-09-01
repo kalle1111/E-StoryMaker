6763dd95eef2f5e189bde0884d454254f7fddac9
Tim <tim.braasch.fl@gmail.com>
2021-09-01 15:44:31 +0200

    Added image to User and removed firstname, lastname and birthday

--- app/src/main/java/hsfl/project/e_storymaker/roomDB/Entities/user/User.kt
+++ app/src/main/java/hsfl/project/e_storymaker/roomDB/Entities/user/User.kt
@@ -8,11 +8,9 @@ import kotlinx.serialization.Serializable
 @SuppressWarnings(RoomWarnings.PRIMARY_KEY_FROM_EMBEDDED_IS_DROPPED)
 data class User(
     @PrimaryKey(autoGenerate = false) val username: String,
-    val firstname: String,
-    val lastname: String,
     val user_description: String,
-    val birthday: String,
     val hashPassword: String,
+    @ColumnInfo(typeAffinity = ColumnInfo.BLOB) val image: ByteArray,
     var cachedTime: Long
 )
 
